eb26a1a150d9c79e21f808533881513c76d788d6
Subarna Pokharel <subarnapokharel252@gmail.com>
2022-06-16 17:13:02 +0545

    Added-reset-button-for-user-to-selct-desired-row-columns

--- script.js
+++ script.js
@@ -5,11 +5,16 @@ const gridItem=document.querySelectorAll('.grid-item');
 
 function createDiv(rows,columns)
 {
+
+    while(document.querySelector("button")!=null)
+    {
+        document.querySelector("button").remove();
+    }
     
     for(let i=0;i<(rows*columns);i++)
     {
         let squareDiv=document.createElement('div');
-        squareDiv.innerText=(i+1);
+        // squareDiv.innerText=(i+1);
         
         squareDiv.style.backgroundColor="white";
         squareDiv.style.height="50px";
@@ -18,18 +23,51 @@ function createDiv(rows,columns)
 
         squareDiv.addEventListener('mouseover',()=>{
             squareDiv.style.backgroundColor="black";
-            squareDiv.style.color="white";
+            
             console.log(squareDiv);
         })
 
-        squareDiv.addEventListener('mouseout',()=>{
-            squareDiv.style.backgroundColor="white";
-            squareDiv.style.color="black";
-            console.log(squareDiv);
-        })
+        
+
+        
+
+        
     }
+    makeButton();
     
 }
 createDiv(16,16);
+
+
+function makeButton()
+{
+    const buttonDiv=document.querySelector('#button');
+
+    const resetButton=document.createElement("button");
+    resetButton.textContent="RESET!";
+    resetButton.style.margin="20px";
+    resetButton.className="rst-btn";
+    buttonDiv.appendChild(resetButton);
+
+
+    resetButton.addEventListener('click',()=>{
+        document.querySelectorAll('.grid-item').forEach(e=>e.remove());
+    
+    let userInputGrid=prompt("Please Enter the number of grid squares per side (MAXIMUM:100)");
+    if(userInputGrid>100)
+    {
+        alert('Error!');
+        return;
+    }
+    rows=userInputGrid;
+    columns=userInputGrid;
+    createDiv(rows,columns);
+    
+    })
+
+
+
+
+}
 
 
