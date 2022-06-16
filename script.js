277d94ecb984583c7782b315310dd84db7e43b8e
Subarna Pokharel <subarnapokharel252@gmail.com>
2022-06-16 06:15:53 +0545

    Added-hover-effect-to-switch-square-box-color

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const container=document.querySelector('#container');
+const gridItem=document.querySelectorAll('.grid-item');
+
 
 
 function createDiv(rows,columns)
@@ -8,9 +10,26 @@ function createDiv(rows,columns)
     {
         let squareDiv=document.createElement('div');
         squareDiv.innerText=(i+1);
-        squareDiv.style.backgroundColor="blue";
+        
+        squareDiv.style.backgroundColor="white";
+        squareDiv.style.height="50px";
         squareDiv.className="grid-item";
         container.appendChild(squareDiv);
+
+        squareDiv.addEventListener('mouseover',()=>{
+            squareDiv.style.backgroundColor="black";
+            squareDiv.style.color="white";
+            console.log(squareDiv);
+        })
+
+        squareDiv.addEventListener('mouseout',()=>{
+            squareDiv.style.backgroundColor="white";
+            squareDiv.style.color="black";
+            console.log(squareDiv);
+        })
     }
+    
 }
 createDiv(16,16);
+
+
